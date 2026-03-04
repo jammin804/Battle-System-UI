a5e0b9422106fe4c7e28bc9f65e133c73f89c820
Mike Harris <jammin804@gmail.com>
2026-03-03 21:43:47 -0500

    Added Turn Order UI

--- Scripts/battle.gd
+++ Scripts/battle.gd
@@ -2,3 +2,13 @@ class_name Battle extends Control
 
 @onready var _options: WindowDefault = %Options
 @onready var _options_menu: Menu = $Options/Options
+
+func _ready() -> void:
+	_options_menu.button_focus(0)
+
+func _on_options_button_focused(button: BaseButton) -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed(button: BaseButton) -> void:
+	print(button.text) # Replace with function body.
